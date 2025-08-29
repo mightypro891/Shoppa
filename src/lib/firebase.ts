90bb83ae491628799d6eae4c1772407bd56962dd
Promise Oyedele <promiseoyedele07@gmail.com>
2025-08-29 04:34:49 +0000

    createErrorInternal node_modules/firebase/node_modules/@firebase/auth/sr

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "lautech-shoppa",
-  "appId": "1:453886364219:web:32454a86f9f23e313de218",
-  "storageBucket": "lautech-shoppa.appspot.com",
-  "apiKey": "AIzaSyAzXF_RkSowk7V5une5S_Hh_eMvM5s-R-Q",
-  "authDomain": "lautech-shoppa.firebaseapp.com",
-  "messagingSenderId": "453886364219"
+  "projectId": "lautech-shoppa-4",
+  "appId": "1:932976563602:web:97305929665a20de735624",
+  "storageBucket": "lautech-shoppa-4.appspot.com",
+  "apiKey": "AIzaSyAz_-_y9WfALgD8dc7sc-5uoqZhjVbOSdE",
+  "authDomain": "lautech-shoppa-4.firebaseapp.com",
+  "messagingSenderId": "932976563602"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
