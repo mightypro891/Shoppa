3cc3097317e40c859b6a855aa4ee09ad301de512
Promise Oyedele <promiseoyedele07@gmail.com>
2025-08-29 04:48:03 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "lautech-shoppa-669b3",
-  "appId": "1:333904583155:web:32a933a8868623758b99d6",
-  "storageBucket": "lautech-shoppa-669b3.appspot.com",
-  "apiKey": "AIzaSyDFf3J_v5vscSTqL6A8o9n2fA9zD9SVO24",
-  "authDomain": "lautech-shoppa-669b3.firebaseapp.com",
-  "messagingSenderId": "333904583155"
+  "projectId": "naija-shoppa",
+  "appId": "1:137647070211:web:b0d277643206bf226ac236",
+  "storageBucket": "naija-shoppa.firebasestorage.app",
+  "apiKey": "AIzaSyCGHpCReIV5n-b4FXynO4NPybvNDTT9c6w",
+  "authDomain": "naija-shoppa.firebaseapp.com",
+  "messagingSenderId": "137647070211"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
