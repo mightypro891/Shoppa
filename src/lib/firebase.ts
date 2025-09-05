5d8725890f4118d2e376655170fc635938ef95cf
Promise Oyedele <promiseoyedele07@gmail.com>
2025-09-05 11:56:07 +0000

    No this is not what I meant make the store like before I ask you to acti

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // This file is intentionally left blank for the prototype version.
 // All Firebase dependencies have been removed.
 // In a real application, this file would contain the Firebase client configuration.
